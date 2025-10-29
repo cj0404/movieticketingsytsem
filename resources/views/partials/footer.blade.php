2b6e38191a376c33288753222c7cc0bb5a7dec50
cj0404 <dumayacatintin@gmail.com>
2025-10-29 16:11:56 +0800

    Update footer.blade.php

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,11 +1,3 @@
-{{-- <footer class="bg-dark text-light py-3 mt-4">
-  <div class="container d-flex justify-content-between">
-    <div>© {{ date('Y') }} CinemaX</div>
-    <div>Built with Laravel & Bootstrap</div>
-  </div>
-</footer> --}}
-
-<footer class="bg-dark text-white text-center py-3 mt-5">
+<footer class="bg-dark text-white text-center py-3 mt-5" style="position: fixed; bottom: 0; width: 100%;">
   <p>&copy; {{ date('Y') }} Movie Theater Ticketing System</p>
 </footer>
-
